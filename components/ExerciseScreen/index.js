b2c0af93a5bcfa8f0a5a64aff55e56ffd44dd3e4
Halie Chalkley <magpieshrike@gmail.com>
2021-05-04 21:29:27 -0400

    instructions

--- components/ExerciseScreen/index.js
+++ components/ExerciseScreen/index.js
@@ -13,27 +13,46 @@ import { StyleSheet, SafeAreaView, FlatList, View } from 'react-native';
 import { Card, Text, Button } from 'react-native-elements';
 import { BackgroundImage } from 'react-native-elements/dist/config';
 
+
+
 function ListScreen({ navigation, route }) {
     const { item, items } = route.params;
 
     let nextItem = items.findIndex((curItem) => curItem == item);
     nextItem = (nextItem + 1) % items.length;
 
+    const renderItem = ({ ...data }) => {
+        return ( 
+            <View style={{ paddingBottom: 20}}>
+                <Text style={ styles.stepTitle }>Step {data.item.key}</Text>
+                <Text style={{ textAlign: "justify" }}>{data.item.step}</Text>
+            </View>
+        );
+    };
+
     return (
         <SafeAreaView style={styles.container}>
             <BackgroundImage source={require('./727.jpg')} style={styles.image}>
-                <Card>
+                <View>
+                <Card >
                 <View style={ styles.strip }></View>
                     <View style={{ paddingHorizontal: 90, paddingBottom: 30 }}>
                         <Card.Title style={styles.title}>{ item.name }</Card.Title>
                         <Card.Divider />
                     </View>
+                    <View style ={{ paddingHorizontal: 30, }}>
+                        <FlatList data={item.instructions} renderItem={renderItem} />
+                    </View>
                 </Card>
-                <Button 
-                    style={styles.button}
-                    title={`Next Exercise: ${items[nextItem].name}`} 
-                    onPress={() => navigation.push('Exercise', { item: items[nextItem], items: items })}
-                />
+                </View>
+                <View style={styles.bottom}>
+                    <Button 
+                        buttonStyle={{ backgroundColor: "#081431", width: 450, paddingHorizontal: 10, paddingVertical:10 }}
+                        titleStyle={{ color: "#41e8e2", fontWeight: "bold" }}
+                        title={`Next Exercise: ${items[nextItem].name}`} 
+                        onPress={() => navigation.push('Exercise', { item: items[nextItem], items: items })}
+                    />
+                </View>
             </BackgroundImage>
         </SafeAreaView>
     )
@@ -47,7 +66,7 @@ const styles = StyleSheet.create({
         flex: 1,
         resizeMode: 'cover',
         alignItems: "center",
-        justifyContent: "center",
+        paddingTop: 30,
       },
     strip:{
         backgroundColor: "#41e8e2",
@@ -58,9 +77,16 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 30,
     },
-    button: {
-        position: "absolute",
-        bottom: 0
+    bottom: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        zIndex: 10
+    },
+    stepTitle: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        paddingBottom: 5,
+        color: "#081431"
     }
   });
 
